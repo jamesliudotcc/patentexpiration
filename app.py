71e81832f2e20cfca74ac8fec6f7483ddcec1d98
James Liu <james@jamesliu.cc>
2018-08-03 17:16:42 -0700

    Working as a webform

--- app.py
+++ app.py
@@ -1,12 +1,35 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, flash, request
+from wtforms import Form, TextField, TextAreaField, validators, StringField, SubmitField
 import patentexp
-app = Flask(__name__)
 
-@app.route('/')
+#DEBUG = True
+app = Flask(__name__)
+app.config['SECRET_KEY'] = '888' 
+
+class ReusableForm(Form):
+    patent = TextField('Patent number:', validators=[validators.required()])
+
+
+@app.route('/', methods=['GET', 'POST'])
 def index():
-    return '<h1>Hello World</h1>'
+    form = ReusableForm(request.form)
+    
+
+    print(form.errors)
+    if request.method == 'POST':
+        patent=request.form['patent']
+        patent_exp_str = patentexp.patent_term_expire(patent)
+        print(patent)
+
+        if form.validate():
+            # Save the coment here.
+            flash(patent_exp_str)
+        else:
+            flash('All the form fields are required. ')
+    return render_template('index.html', form=form)
 
 @app.route('/patent/<patent_number>')
 def patent_number(patent_number):
     patent_exp_str = patentexp.patent_term_expire(patent_number)
-    return '<p>{}</p>'.format(patent_exp_str)
+    return '<p>{}</p> <p>Patent term disclaimer and patent term extension based on regulatory review is not supported.'.format(patent_exp_str)
+
